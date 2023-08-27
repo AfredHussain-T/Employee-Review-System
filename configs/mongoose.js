ea1ed0f501d513b22fad3e531142f6ddc45aa6d7
AfredHussain-T <afredhussain69@gmail.com>
2023-08-28 00:01:27 +0530

    need to be deployed on render

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/emp-rev-system');
+// mongoose.connect('mongodb://127.0.0.1:27017/emp-rev-system');
+mongoose.connect('mongodb+srv://afredhussain69:RcIdyuqWCvshqBQm@cluster0.jv06oti.mongodb.net/emp-rev-sys');
 
 const connection = mongoose.connection;
 
